9f2652063b5df4d522daf6652020d121f55cbaf5
Widhi Mahaputra <widhimp@gmail.com>
2025-07-27 22:33:13 +0800

    fix build error #2

--- app/anggota/pinjaman_member/page.tsx
+++ app/anggota/pinjaman_member/page.tsx
@@ -1,13 +1,16 @@
-import Header from "@/components/customs/header";
-import Footer from "@/components/customs/footer";
-import PinjamanMemb from "@/components/customs/M_Pinjaman";
+import Header from '@/components/customs/header';
+import Footer from '@/components/customs/footer';
+import PinjamanMemb from '@/components/customs/M_Pinjaman';
+import { Suspense } from 'react';
 
-export default function Page(){
-    return(
-        <>
-        <Header/>
-        <PinjamanMemb/>
-        <Footer/>
-        </>
-    )
+export default function Page() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <>
+        <Header />
+        <PinjamanMemb />
+        <Footer />
+      </>
+    </Suspense>
+  );
 }
