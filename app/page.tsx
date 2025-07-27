b97d6c651db57ed704f82965863cd516551444ec
Widhi Mahaputra <widhimp@gmail.com>
2025-07-27 22:01:04 +0800

    trigger deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import { Dashboard } from "@/components/custom/dashboard";
-import { Header } from "@/components/custom/header";
-
-
+import { Dashboard } from '@/components/custom/dashboard';
+import { Header } from '@/components/custom/header';
 
 export default function Page() {
   return (
     <>
-    <Header />
-    <Dashboard />
+      <Header />
+      <Dashboard />
     </>
-  )
+  );
 }
