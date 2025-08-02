f02fe220eee48fd53ee3c14dbf4737a47592d4d9
unknown <triadiswas31@gmail.com>
2025-08-02 11:09:09 +0800

    PK 01-19

---
+++ app/anggota/pinjaman_member/page.tsx
@@ -0,0 +1,16 @@
+import Header from '@/components/customs/layouts/header';
+import Footer from '@/components/customs/layouts/footer';
+import PinjamanMemb from '@/components/customs/anggota/Pinjaman';
+import { Suspense } from 'react';
+
+export default function Page() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <>
+        <Header />
+        <PinjamanMemb loans={[]} />
+        <Footer />
+      </>
+    </Suspense>
+  );
+}
