063db908ffb8f01b805bc5f9c7aaa5c948dc05db
unknown <triadiswas31@gmail.com>
2025-08-09 13:11:52 +0800

    Kelupaan animasi loading di dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Header from "@/components/customs/layouts/header";
 import Welcome from "@/components/customs/layouts/welcome";
 import Habis from "@/components/customs/dashboard/stok-habis";
@@ -6,11 +8,25 @@ import Pengembali from "@/components/customs/dashboard/pengembalian";
 import Goodbye from "@/components/customs/layouts/goodbye";
 import Footer from "@/components/customs/layouts/footer";
 import { fetchBooks, fetchLoans, fetchReturn } from "@/lib/api";
+import useLoading from "@/components/customs/loading/useLoading";
+import Loading from "@/components/customs/loading/Loading";
+import { useState, useEffect } from "react";
 
   export default async function Page() {
-    const { data: books } = await fetchBooks();
-    const { data: loans } = await fetchLoans();
-    const { data: returns } = await fetchReturn();
+    const loading = useLoading();
+    const [books, setBooks] = useState([]);
+    const [loans, setLoans] = useState([]);
+    const [returns, setReturns] = useState([]);
+
+    useEffect(() => {
+      fetchBooks().then(({ data: books }) => setBooks(books));
+      fetchLoans().then(({ data: loans }) => setLoans(loans));
+      fetchReturn().then(({ data: returns }) => setReturns(returns))
+    }, [])
+
+    if(loading){
+      return <Loading/>
+    }
 
     return (
       <>
