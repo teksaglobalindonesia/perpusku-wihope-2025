cd9030ceb84e2f11897b73939455102809e47b9a
unknown <madealvin1333@gmail.com>
2025-08-11 09:37:04 +0800

    sudah pindah perpusku-strapi

--- perpusku-strapi/config/server.ts
+++
@@ -1,9 +0,0 @@
-import { EnvFn } from '../types/generated/env';
-
-export default ({ env }: { env: EnvFn }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS')
-  }
-});
--- perpusku-strapi/types/generated/env.d.ts
+++
@@ -1,7 +0,0 @@
-// types/env.d.ts
-export type EnvFn = {
-  (key: string, defaultValue?: any): any;
-  int: (key: string, defaultValue?: number) => number;
-  bool: (key: string, defaultValue?: boolean) => boolean;
-  array: (key: string, defaultValue?: string[]) => string[];
-};
