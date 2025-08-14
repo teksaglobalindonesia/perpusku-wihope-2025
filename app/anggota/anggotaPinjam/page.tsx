9b08d2d00b662e0aaaa5a4b36eeef74ddaa6ea59
unknown <madealvin1333@gmail.com>
2025-08-14 14:31:46 +0800

    fix tambah buku kategori anggota

--- app/anggota/anggotaPinjam/page.tsx
+++ app/anggota/anggotaPinjam/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import AnggotaMinjem from '@/components/custom/member/anggotaMinjem';
 
 export default function Page() {
-  return <AnggotaMinjem />;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <AnggotaMinjem />
+    </Suspense>
+  );
 }
